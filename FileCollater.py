2d52ad8284db89aff59ea25b42ee25d55f98ab93
Chris Rodgers <chris.rodgers@berkeley.edu>
2011-06-05 14:48:28 -0700

    Adding ability to collate image results across directories, and also automatically spike detect

---
+++ FileCollater.py
@@ -0,0 +1,28 @@
+class FileCollater:
+    def __init__(self, data_dir_list, filenamestring, output_dir,
+        output_file_name=None):
+        self.data_dir_list = data_dir_list
+        self.filenamestring = filenamestring
+        self.output_dir = output_dir
+        self.output_file_name = output_file_name
+        
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+        
+        if self.output_file_name is None:
+            self._generate_output_file_name()
+    
+    def copy_files(self):       
+        for n, data_dir in enumerate(data_dir_list):
+            tgt_name_list = glob.glob(os.path.join(data_dir, filenamestring))
+            for tgt_name in tgt_name_list:
+                dst_name = os.path.join(output_dir, 
+                    self.output_file_name[n] + os.path.split(tgt_name)[1])
+                shutil.copyfile(tgt_name, dst_name)
+    
+    def _generate_output_file_name(self):
+        self.output_file_name = list()
+        for data_dir in self.data_dir_list:
+            split_data_dir = data_dir.split(os.path.sep)
+            self.output_file_name.append(\
+                split_data_dir[-2] + '_' + split_data_dir[-1] + '_')
