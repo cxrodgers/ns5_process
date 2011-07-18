d68445456a87127dc261d81a437617b46e8b8230
Chris Rodgers <chris.rodgers@berkeley.edu>
2011-07-18 16:54:45 -0700

    Adding ability to read KK files

--- KlustaKwikIO.py
+++ KlustaKwikIO.py
@@ -1,9 +1,126 @@
+# Iterate through all neurons in OE db and dump spiketimes
+# We will use the KlustaKwik compatible Feature File format
+# Ref: http://klusters.sourceforge.net/UserManual/data-files.html
+# Begin format specification (lightly modified from above):
+"""
+The Feature File
+
+Generic file name: base.fet.n
+
+Format: ASCII, integer values
+
+The feature file lists for each spike the PCA coefficients for each
+electrode, followed by the timestamp of the spike (more features can
+be inserted between the PCA coefficients and the timestamp). 
+The first line contains the number of dimensions. 
+Assuming N1 spikes (spike1...spikeN1), N2 electrodes (e1...eN2) and
+N3 coefficients (c1...cN3), this file looks like:
+
+nbDimensions
+c1_e1_spike1   c2_e1_spike1  ... cN3_e1_spike1   c1_e2_spike1  ... cN3_eN2_spike1   timestamp_spike1
+c1_e1_spike2   c2_e1_spike2  ... cN3_e1_spike2   c1_e2_spike2  ... cN3_eN2_spike2   timestamp_spike2
+...
+c1_e1_spikeN1  c2_e1_spikeN1 ... cN3_e1_spikeN1  c1_e2_spikeN1 ... cN3_eN2_spikeN1  timestamp_spikeN1
+
+The timestamp is expressed in multiples of the sampling interval. For
+instance, for a 20kHz recording (50 microsecond sampling interval), a
+timestamp of 200 corresponds to 200x0.000050s=0.01s from the beginning
+of the recording session.
+
+Notice that the last line must end with a newline or carriage return. 
+"""
+
 import numpy as np
 import glob
 import matplotlib.mlab as mlab
 from SpikeTrainContainers import MultipleUnitSpikeTrain
 import os.path
 
+
+class KlustaKwikIO(object):
+    def __init__(self, filename=None):
+        self.filename = filename
+        self._fetfiles = dict()
+        self._clufiles = dict()
+    
+    def _new_group(self, id_group, nbClusters):
+        self._fetfiles[id_group] = file(self.filename + \
+            ('.fet.%d' % id_group), 'w')
+        self._clufiles[id_group] = file(self.filename + \
+            ('.clu.%d' % id_group), 'w')
+        
+        self._fetfiles[id_group].write("0\n") # Number of features
+        self._clufiles[id_group].write("%d\n" % nbClusters)
+    
+    def _get_file_handles(self, st):
+        try:
+            id_group = st.recordingpoint.group
+        except AttributeError:
+            id_group = -1
+        
+        try:
+            fetfile = self._fetfiles[id_group]
+            clufile = self._clufiles[id_group]
+        except KeyError:
+            self._new_group(id_group, 0)
+            fetfile = self._fetfiles[id_group]
+            clufile = self._clufiles[id_group]
+        
+        return (fetfile, clufile)
+    
+    def _close_all_files(self):
+        for val in self._fetfiles.values():
+            val.close()
+        
+        for val in self._clufiles.values():
+            val.close()
+    
+    def _make_all_file_handles(self, neuron_list):
+        # Get the tetrode (group) of each neuron (cluster)
+        id_groups = list()
+        for n in neuron_list:
+            try:
+                id_group = n._spiketrains[0].recordingpoint.group
+            except (IndexError, AttributeError):
+                id_group = -1
+            
+            if id_group is None:
+                id_group = n._spiketrains[0].recordingpoint.id
+            
+            id_groups.append(id_group)
+
+        # Make new file handles for each group
+        for id_group in np.unique(id_groups):
+            self._new_group(id_group, nbClusters=id_groups.count(id_group))        
+    
+    def write_block(self, block):     
+        # Make all file handles
+        neuron_list = block._neurons
+        #1/0
+        self._make_all_file_handles(neuron_list)
+        
+        # Will contain info about each segment
+        segment_info = list()
+        
+        # Iterate through segments in this block
+        for seg in block.get_segments():
+            # Write each spiketrain of the segment
+            for st in seg.get_spiketrains():  
+                # Get file handles for this spiketrain using its group
+                fetfile, clufile = self._get_file_handles(st)
+                
+                # Write each spike of the spiketrain                
+                for stt in np.rint(st.spike_times * 30e3).astype(int):
+                    # Would normally write features here
+                    # But currently features are not stored
+                    
+                    # Write spike time in samples
+                    fetfile.write("%d\n" % stt)
+                    
+                    # Write neuron id
+                    clufile.write("%s\n" % st.name)
+        
+        self._close_all_files()
 
 class KK_loader(object):
     """Loads spike info from KlustaKwik-compatible files."""
