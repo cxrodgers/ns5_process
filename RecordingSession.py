0b2d98ec049c27b1949aa5f54a449950c00c1208
Chris Rodgers <chris.rodgers@berkeley.edu>
2012-06-05 15:55:22 -0700

    klusters compatibility

--- RecordingSession.py
+++ RecordingSession.py
@@ -517,17 +517,15 @@ class RecordingSession:
                 ch2rpid[ch] = rp_id
 
         # Copy old segments over to new block
+        if verbose:
+            printnow("populating segments ... this may take a while")
         for old_seg in raw_block._segments:
-            if verbose:
-                printnow("creating segment")
             # Create a new segment in the new block with the same name
             new_seg = OE.Segment(name=old_seg.name, info=old_seg.info,
                 id_block=spike_block.id)
             new_seg.save(session=session)
 
             # Populate with filtered signals
-            if verbose:
-                printnow("populating segment")
             new_seg = self._create_spike_seg(old_seg, new_seg, CAR, session, 
                 ch2rpid)
 
@@ -932,6 +930,117 @@ class RecordingSession:
             spikesorter.save_to_klusters(basename=save_to_klusters)
     
         return spikesorter
+
+    def prep_for_klusters(self, override_path=None, verbose=False, force=False):
+        """Prepare fet and spk files for processing with Klusters.
+        
+        override_path : Full path to where klusters file exists.
+            If None, use self.last_klusters_dir()
+        
+        Renames to be 1-based instead of 0-based
+        Creates xml file
+        """
+        
+        if override_path is None:
+            override_path = self.last_klusters_dir()
+        kdir = override_path
+        
+        # Determine whether files are 1-based or 0-based
+        fet0 = glob.glob(os.path.join(kdir, '*.fet.0'))
+        clu0 = glob.glob(os.path.join(kdir, '*.clu.0'))
+        
+        if len(fet0) == 1 and len(clu0) == 1:
+            renumber = True
+        elif len(fet0) == 0 and len(clu0) == 0:
+            renumber = False
+        else:
+            print "error: cannot renumber fet and clu files"
+            return
+        
+        if renumber:
+            # get file names and numbers
+            fns = sorted(os.listdir(kdir), reverse=True)
+            if 'backup' in fns: fns.remove('backup')
+            fnums = map(lambda fn: int(fn.split('.')[-1]), fns)
+
+            # Rename each one
+            for fn, fnum in zip(fns, fnums):
+                newfn = os.path.splitext(fn)[0] + '.%d' % (fnum + 1)
+                assert not os.path.exists(os.path.join(kdir, newfn))
+                
+                os.rename(os.path.join(kdir, fn), os.path.join(kdir, newfn))
+        
+        newxmlname = os.path.join(kdir, self.session_name + '.xml')
+        if not force and os.path.exists(newxmlname):
+            if verbose:
+                print "%s already exists" % newxmlname
+        else:
+            groups = self.read_channel_groups()
+            self.write_klusters_xml_file(newxmlname, groups)
+
+    def write_klusters_xml_file(self, filename, groups=None, nbits=16, 
+        voltage_range=20, amplification=1000, offset=2048, n_samples=24, 
+        peak_sample_index=11, n_features=8):
+        """Writes the xml file with sorting parameters that Klusters needs.
+        
+        This includes information about waveforms and features. These will be
+        associated with the fet and clu files in the same directory:
+        the first channel group is associated with *.fet.1 and *.clu.1, etc.
+        
+        groups : a list of lists. Each sublist is the channel numbers in
+            that group. eg [[1,2,3], [4,5,6,7]]
+        
+        Here I write the actual channel numbers but I'm not sure Klusters
+        actually uses them. It probably just needs to know how many channels
+        per group.
+        """
+        from lxml import etree
+
+        # Acquisition system parameters, including the binary specification
+        # for the spk files
+        p = etree.Element("parameters")
+        acs = etree.Element("acquisitionSystem")
+        p.append(acs)
+        el = etree.Element("nBits"); el.text = '16'
+        acs.append(el)
+        el = etree.Element("samplingRate")
+        el.text = str(int(self.get_sampling_rate()))
+        acs.append(el)
+        el = etree.Element("voltageRange"); el.text = str(voltage_range)
+        acs.append(el)
+        el = etree.Element("amplification"); el.text = str(amplification)
+        acs.append(el)
+        el = etree.Element("offset"); el.text = str(offset)
+        acs.append(el)
+        
+        # Channel groups
+        sd = etree.Element("spikeDetection")
+        cg = etree.Element("channelGroups")
+        for group in groups:
+            g = etree.Element("group")
+            c = etree.Element("channels")
+            for cnum in group:
+                cc = etree.Element("channel")
+                cc.text = str(cnum)
+                c.append(cc)
+            g.append(c)
+            cg.append(g)
+            
+            el = etree.Element("nSamples"); el.text = str(n_samples)
+            g.append(el)
+            el = etree.Element("peakSampleIndex")
+            el.text = str(peak_sample_index)
+            g.append(el)
+            el = etree.Element("nFeatures"); el.text = str(n_features)
+            g.append(el)
+        sd.append(cg)
+        p.append(sd)        
+        
+        # Output xml
+        fi = file(filename, 'w')
+        fi.write(etree.tostring(p, pretty_print=True))
+        fi.close()
+
 
     def spiketime_dump(self):
         # Dump in klustakwik format
